deaa7ca9a67cd2d2bed308ca367faf24b39899e9
github username <github email address>
2020-09-08 14:09:07 -0700

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,17 +1,23 @@
 def using_until
-  levitation_force = 6
+  levitation_force = 6 #DONT DELETE
+  
   # 1. condition to be met
   # 2. stuff to do until the condition is met
-  # 3. how do we meet that condition? (altering a counter?)
+  # 3. how do we meet that condition? (altering a counter?)- if we never meet the condition we'll have an infinite loop
 
-  # In psuedocode:
-  # until the levitation_force is equal to 10
-    # print "Wingardium Leviosa"
-    # increment the levitation_force by 1
+  # go over basic syntax
+  # until condition (is true-implicit)
+  # do something
+  # end
 
-  until levitation_force == 10
+  # In psuedocode:-follow 1, 2, 3 above
+  # 1.) until the levitation_force is equal to 10
+    # 2.) print "Wingardium Leviosa"
+    # 3.) increment the levitation_force by 1
+
+  until levitation_force == 10 # equality operator v assignment
     # levitation_force = levitation_force + 1
     puts "Wingardium Leviosa"
-    levitation_force += 1
+    levitation_force += 1 #levitation_force = levitation_force + 1
   end
 end
--- lib/while.rb
+++ lib/while.rb
@@ -1,23 +1,39 @@
+# look over readme, but will mainly be relying on tests
+# will then go test by test...also look at spec file
+# really try for interaction-ask for their guesses before doing
+# review example of basic while loop
+# difference between while and until
+
+#fix errors little by little-i.e. just no method error, then read new error
+# output is telling me it wants you to puts this to the console
+
 def using_while
   levitation_force = 6
   # 1. condition to be met
   # 2. stuff to do while the condition is true
-  # 3. how do we meet that condition?
+  # 3. how do we meet that condition? - if we never meet the condition we'll have an infinite loop
+
+  # go over basic syntax
+  # while condition (is true-implicit)
+  # do something
+  # end
 
   # In psuedocode:
   # while the levitation_force is less than 10
     # print "Wingardium Leviosa"
     # increment the levitation_force by 1
 
+  # what condition to use?
+  # while levitation_force <= 10
+  # ^This will print it 10 times, but we need 9!-can try both-review what counter will be on each loop-can look in test- + means extra/more than expected
+
   while levitation_force < 10
     puts "Wingardium Leviosa"
     levitation_force += 1
     
-  # while levitation_force <= 10
-  # ^This will print it 5 times, but we need 4!
-    
   end
 end
+#once write, review what did
 
 # You can run `learn --f-f to only see the first failing test`
 # You can run `clear` to clear your terminal
